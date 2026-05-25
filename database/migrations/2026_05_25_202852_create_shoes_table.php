0e542de31776879121505d22f43924849ce20b7f
kennethamerafghan <kennethamerjualo@gmail.com>
2026-05-26 04:45:12 +0800

    Fix migration order

--- database/migrations/2026_05_25_202852_create_shoes_table.php
+++ database/migrations/2026_05_25_202852_create_shoes_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('brand');
             $table->decimal('price', 8, 2);
-            $table->string('size')->nullable();
             $table->timestamps();
         });
     }
